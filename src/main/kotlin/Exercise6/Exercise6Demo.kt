8a0227b0ac766e694b84e1f5166462ab7887d751
joao <joao.ranger@gmail.com>
2022-04-20 14:53:40 +0100

    start of exercise 6

---
+++ src/main/kotlin/Exercise6/Exercise6Demo.kt
@@ -0,0 +1,12 @@
+package Exercise6
+
+
+fun Exercise6Demo() {
+    println("_\t_Exercise6_\t_")
+
+
+}
+
+
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
-import Exercise4.Exercise4Demo
-import Exercise5.Exercise5Demo
+import Exercise6.Exercise6Demo
 
 fun main() {
     //Exercise1Demo()
     //Exercise2Demo()
     //Exercise3Demo()
     //Exercise4Demo()
-    Exercise5Demo()
+    //Exercise5Demo()
+    Exercise6Demo()
 }
